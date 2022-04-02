20a399a9463e466eeb8f3508f735217446cb0f1c
Jose Junior <jose.junior@porlande.com>
2022-04-02 23:41:24 +0100

    login packages installed

--- to_na_lei/lib/main.dart
+++ to_na_lei/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:to_na_lei/pages/ui/auth/login_page.dart';
+
 import 'package:to_na_lei/pages/ui/splash/splash.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
